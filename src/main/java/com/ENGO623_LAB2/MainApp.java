ba4bebf089edce714af62bb6dcb76f7906b0f1b6
Naman Agarwal <naman4u13@gmail.com>
2023-02-22 21:39:37 -0700

    Refactored and documented the code

--- src/main/java/com/ENGO623_LAB2/MainApp.java
+++ src/main/java/com/ENGO623_LAB2/MainApp.java
@@ -23,14 +23,15 @@ public class MainApp {
 		try {
 
 			System.out.println("STARTED");
-			File output = new File("C:\\Users\\naman.agarwal\\Documents\\EclipseProjects\\ENGO_623_LAB2\\src\\main\\resources\\result.txt");
+			File output = new File("C:\\Users\\naman.agarwal\\Downloads\\my work\\ENGO 623\\Lab2\\result4.txt");
 			PrintStream stream;
 			stream = new PrintStream(output);
 			System.setOut(stream);
-			ArrayList<double[]> ltn_down = Parser.getLTNData("x_LN100_down.ins");
-			ArrayList<double[]> ltn_up = Parser.getLTNData("x_LN100_up.ins");
-			ArrayList<double[]> mems_down = Parser.getMEMSData("x_adi_down.txt");
-			ArrayList<double[]> mems_up = Parser.getMEMSData("x_adi_up.txt");
+			// Parsing IMU's binary and text files
+			ArrayList<double[]> ltn_up = Parser.getLTNData("x_LN100_down.ins");
+			ArrayList<double[]> ltn_down = Parser.getLTNData("x_LN100_up.ins");
+			ArrayList<double[]> mems_up = Parser.getMEMSData("x_adi_down.txt");
+			ArrayList<double[]> mems_down = Parser.getMEMSData("x_adi_up.txt");
 			System.out.println("LTN and MEMS file up and down record size");
 			System.out
 					.println(ltn_down.size() + "  " + ltn_up.size() + "  " + mems_down.size() + "  " + mems_up.size());
@@ -38,17 +39,25 @@ public class MainApp {
 			double[][] ltn_up_mean_var = new double[6][2];
 			double[][] mems_down_mean_var = new double[6][2];
 			double[][] mems_up_mean_var = new double[6][2];
+			// Computing mean and variance error of the mean
 			for (int i = 1; i < 7; i++) {
 				ltn_down_mean_var[i - 1] = getMeanVar(ltn_down, i);
 				ltn_up_mean_var[i - 1] = getMeanVar(ltn_up, i);
 				mems_down_mean_var[i - 1] = getMeanVar(mems_down, i);
 				mems_up_mean_var[i - 1] = getMeanVar(mems_up, i);
 			}
+			// Computing Bias and Scale factors along with their standard error of the mean 
 			double[] ltn_x_acc_bias_scale = getAccBiasScale(ltn_down_mean_var[3], ltn_up_mean_var[3]);
 			double[] mems_x_acc_bias_scale = getAccBiasScale(mems_down_mean_var[3], mems_up_mean_var[3]);
 			double[] ltn_x_gyro_bias_scale = getGyroBiasScale(ltn_down_mean_var[0], ltn_up_mean_var[0]);
 			double[] mems_x_gyro_bias_scale = getGyroBiasScale(mems_down_mean_var[0], mems_up_mean_var[0]);
-
+			// Square rooting Variance to Std. dev
+			for (int i = 0; i < 6; i++) {
+				ltn_down_mean_var[i][1] = Math.sqrt(ltn_down_mean_var[i][1]);
+				ltn_up_mean_var[i][1] = Math.sqrt(ltn_up_mean_var[i][1]);
+				mems_down_mean_var[i][1] = Math.sqrt(mems_down_mean_var[i][1]);
+				mems_up_mean_var[i][1] = Math.sqrt(mems_up_mean_var[i][1]);
+			}
 			for (int i = 0; i < 6; i++) {
 				System.out.println("LTN down " + i);
 				print(ltn_down_mean_var[i]);
@@ -82,7 +91,7 @@ public class MainApp {
 		double scaleMean = (down[0] - up[0] - (2 * g)) / (2 * g);
 		double biasVar = (down[1] + up[1]) / 4;
 		double scaleVar = (down[1] + up[1]) / (4 * g * g);
-		return new double[] { biasMean, Math.sqrt(biasVar), scaleMean, Math.sqrt(scaleVar) };
+		return new double[] {biasMean, Math.sqrt(biasVar), scaleMean, Math.sqrt(scaleVar) };
 	}
 
 	private static double[] getGyroBiasScale(double[] down, double[] up) {
@@ -110,7 +119,7 @@ public class MainApp {
 		for (int i = 0; i < n; i++) {
 			System.out.print(data[i] + "  ");
 		}
-		System.out.println();
+		System.out.println("\n");
 	}
 }
 
